d2ee6fa71b95b35c81b0e273b016485ce0a0c8e0
ercantekeli <ercantekeli@outlook.com>
2022-05-18 23:03:39 +0300

    last commit

--- Django/05-Model-2/fscohort/models.py
+++ Django/05-Model-2/fscohort/models.py
@@ -3,7 +3,7 @@ from ssl import ALERT_DESCRIPTION_ACCESS_DENIED
 from django.db import models
 
 class Student(models.Model):
-
+    
     YEAR_IN_SCHOOL_CHOICES = [
         ("FR", 'Freshman'),
         ("SP", 'Sophomore'),
@@ -11,7 +11,6 @@ class Student(models.Model):
         ("SR", 'Senior'),
         ("GRD", 'Graduate'),
     ]
-    
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     number = models.IntegerField()
@@ -21,9 +20,13 @@ class Student(models.Model):
     update_date = models.DateTimeField(auto_now=True)
     year_in_school = models.CharField(max_length=3, choices=YEAR_IN_SCHOOL_CHOICES, default="FR")
     
+    
     def __str__(self):
         return f"{self.first_name} - {self.last_name}"
     
     class Meta:
         ordering = ["number"]
         verbose_name_plural = "Öğrenciler"
+        
+
+        
--- Django/05-Model-2/fscohort/urls.py
+++ Django/05-Model-2/fscohort/urls.py
@@ -4,3 +4,4 @@ from .views import home
 urlpatterns = [
     path('', home, name="home"),
 ]
+
