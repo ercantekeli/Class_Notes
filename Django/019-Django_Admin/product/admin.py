28dfd287ed7c0afb524c88d2458618a554e92847
ercantekeli <ercantekeli@outlook.com>
2022-06-09 20:34:13 +0300

    last commit

---
+++ Django/019-Django_Admin/product/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Product)
+
+admin.site.site_title = "Clarusway Title"
+admin.site.site_header = "Clarusway Admin Portal"  
+admin.site.index_title = "Welcome to Clarusway Admin Portal"
