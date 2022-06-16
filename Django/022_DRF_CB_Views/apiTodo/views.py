aebcfc09187f47b30467eb484c7dc876d011b264
ercantekeli <ercantekeli@outlook.com>
2022-06-16 18:53:11 +0300

    last commit

--- Django/022_DRF_CB_Views/apiTodo/views.py
+++ Django/022_DRF_CB_Views/apiTodo/views.py
@@ -11,6 +11,9 @@ from .serializers import TodoSerializer
 
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework import mixins, viewsets
+from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.decorators import action
 
 # Create your views here.
 def home(request):
@@ -88,44 +91,83 @@ def todoDelete(request, pk):
     
 #! Class Based Views
 
-class TodoList(APIView):
+# class TodoList(APIView):
     
-    def get(self, request):
-        todos = Todo.objects.all()
-        serializer = TodoSerializer(todos, many=True)
-        return Response(serializer.data)
+#     def get(self, request):
+#         todos = Todo.objects.all()
+#         serializer = TodoSerializer(todos, many=True)
+#         return Response(serializer.data)
     
-    def post(self, request):
-        serializer = TodoSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-        return Response(serializer.data)
+#     def post(self, request):
+#         serializer = TodoSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#         return Response(serializer.data)
     
 
-class TodoDetail(APIView):
+# class TodoDetail(APIView):
     
-    def get_obj(self, id):
-        todo = get_object_or_404(Todo, id=id)
+#     def get_obj(self, id):
+#         todo = get_object_or_404(Todo, id=id)
     
-    def get(self, request, id):
-        todo = self.get_obj(id)
-        serializer = TodoSerializer(todo)
-        return Response(serializer.data)
-    
-    
-    def put(self, request, id):
-        todo =  self.get_obj(id)
-        serializer = TodoSerializer(instance=todo, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def get(self, request, id):
+#         todo = self.get_obj(id)
+#         serializer = TodoSerializer(todo)
+#         return Response(serializer.data)
     
     
-    def delete(self, request, id):
-        todo =  self.get_obj(id)
-        todo.delete()
-        data = {
-            "message" : "Todo successfully deleted"
+#     def put(self, request, id):
+#         todo =  self.get_obj(id)
+#         serializer = TodoSerializer(instance=todo, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+#     def delete(self, request, id):
+#         todo =  self.get_obj(id)
+#         todo.delete()
+#         data = {
+#             "message" : "Todo successfully deleted"
+#         }
+#         return Response(data, status=status.HTTP_204_NO_CONTENT)
+
+
+# class TodoList(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView):
+#     queryset = Todo.objects.all()
+#     serializer_class = TodoSerializer
+    
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+    
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+    
+    
+    
+    
+# class TodoListCreate(ListCreateAPIView):
+#     queryset = Todo.objects.all()
+#     serializer_class = TodoSerializer
+    
+#     # def perform_create(self, serializer):
+#     #     serializer.save(user=self.request.user)
+    
+# class TodoGetUpdateDelete(RetrieveUpdateDestroyAPIView):
+#     queryset = Todo.objects.all()
+#     serializer_class = TodoSerializer
+#     lookup_field = "id"
+    
+
+class TodoMVS(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+        
+    @action(methods=["GET"], detail=False)
+    def todo_count(self, request):
+        todo_count = Todo.objects.filter(done=False).count()
+        count = {
+            'undo-todos': todo_count
         }
-        return Response(data, status=status.HTTP_204_NO_CONTENT)
+        return Response(count)
